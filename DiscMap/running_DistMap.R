d3770d33393f45e32cf73c5e88ec995f4b8d263f
Melanie I. Worley, Ph.D <40185205+MelWorley@users.noreply.github.com>
2021-03-28 18:08:10 -0700

    Update running_DistMap.R

--- DiscMap/running_DistMap.R
+++ DiscMap/running_DistMap.R
@@ -1,7 +1,15 @@
 # R code is for generating a virtual disc model using Distmap (Karaiskos et al., 2017 - https://github.com/rajewsky-lab/distmap) 
 
-# will need a geometry file (supplemental file 4)
+# will need a geometry file (supplemental file 4) : this will have x,y,z coordinate for the location of the reference gene expression patterns 
+#                 x   y  z
+#        1    -11.5  38  0
+#        2    -10.5  38  0
+#        3     -9.5  38  0
+
 # reference expression patterns (supplemental file 5) 
+#        For the following genes: Ance, ap, brk, caup, ci, ct, Dad, Dll, Doc1, dpp, dve, en, eya, eyg, Fas3, fj, hh, Him, hth, htl, mirr, Nrt, 
+#        nub, pdm2, pnr, ptc, rho, rn, salm, Sox15, SPARC, ths, tsh, tup, twi, Ubx, vg, vkg, vvl, wg, Wnt6, zfh2
+
 # and single-cell RNA seq data (we used imputed values from scVI for mapping the cells to the locations). 
 # The cells will be mapped to the reference patterns, and then can be used to generate virtual in situ or predicted gene expression patterns. 
 
@@ -31,3 +39,5 @@ dmDiscImputed <- mapCells(object = dmDiscImputed)
 # gene of interest 
 gene = gene 
 VISH <- computeVISH( dmDiscImputed, gene, threshold = 0)
+
+# this virtual in situ can then be plotted on the same geometry as povided above (see plottingDiscmap.R) 
