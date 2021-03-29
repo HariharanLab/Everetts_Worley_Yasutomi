efd98def01a64711fdcecde044e93d34b88e7f3c
Nicholas-Everetts <NickEv2011@gmail.com>
2021-03-28 17:30:42 -0700

    Update running_DistMap.R

--- DiscMap/running_DistMap.R
+++ DiscMap/running_DistMap.R
@@ -11,28 +11,23 @@
 #DistMap (Karaiskos et al., 2017 - https://github.com/rajewsky-lab/distmap) 
 library(DistMap)
 
+#load in the geometry coordinates (supplemental_file_4.csv), the coordinates for reference genes (supplemental_file_5.csv), and
+#the imputed expression data from scVI.
+geometryfile = as.matrix(read.csv("./supplemental_file_4.csv", row.names = 1, stringsAsFactor = FALSE))
+discExp = as.matrix(read.csv("./supplemental_file_5.csv", header = TRUE, row.names = 1, stringsAsFactor = FALSE))
+imputedData = as.matrix(read.csv(path_to_imputed_data, stringsAsFactor = FALSE))
 
-geometryfile = "~/supplemental_file_4.csv"
-discExp = "~/supplemental_file_5.csv"
-sc_object = "~/object_imputed"
-
-imputedData = as.matrix(sc_object@assays$scVIxNB_Imputed@data)
-
-# Running DistMap 
+# Builing DistMap model 
 
 dmDiscImputed = new("DistMap",
          data = imputedData,
          insitu.matrix = discExp,
          geometry= geometryfile)
-
 dmDiscImputed <- binarizeSingleCellData(object = dmDiscImputed, quantiles = seq(0, 0.5, 0.01))
-
 dmDiscImputed <- mapCells(object = dmDiscImputed)
-
 
 # to calculate virtual in situ:
 
 # gene of interest 
 gene = gene 
-
 VISH <- computeVISH( dmDiscImputed, gene, threshold = 0)
