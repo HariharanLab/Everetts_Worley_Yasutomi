27026dad32d3b202252d6168732313ddf9afcbe6
Melanie I. Worley, Ph.D <40185205+MelWorley@users.noreply.github.com>
2021-03-28 18:24:19 -0700

    Update plottingDiscmap.R

--- DiscMap/plottingDiscmap.R
+++ DiscMap/plottingDiscmap.R
@@ -1,15 +1,36 @@
+## Here is code for visulizing the predicted expression patterns from the Virtual Wing disc (from the DiscMap model) 
 
-gene = "pyr"
 
-VISH <- computeVISH( dmDiscMyoImputed  , gene, threshold = 0)
+# import libraries 
+library(DistMap)
+library(ggplot2)
+library(gridExtra)
+library(viridis)
+viridis = viridis(n = 16, begin = 0, end = 1, option = "D")
 
+# need to have DistMap model loaded and the geometry from supplemental_file_4:  
+dmDiscImputed = readRDS( file = paste0("dmDiscImputed", ".rds")) # load DistMap model
+geometryfile = as.matrix(read.csv("./supplemental_file_4.csv", row.names = 1, header = TRUE, stringsAsFactor = FALSE))
+
+#Functions for visulization
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+pickcolor <- function(Vish_data, lowlim, uplim){
+  range01(pmax(pmin(range01(Vish_data), uplim), lowlim))
+}
+
+
+# gene of interest 
+gene = 'pyr'
+VISH <- computeVISH( dmDiscImputed  , gene, threshold = 0)
+
+# This changes the min/max of the in situ.  
 VISHcolor = pickcolor(VISH, 0.5, 1)
-toGraph <- as.data.frame(cbind(geometry, VISHcolor))
 
+#Create a data fram of geometry and in situ data togeter 
+toGraph <- as.data.frame(cbind(geometryfile, VISHcolor))
 Nzlayers <- unique(toGraph$z)
 plotsHere <- list()
 counter = 1 
-
 for(n in Nzlayers){
   tosave <- toGraph[toGraph$z == n,]
   p1 <- ggplot(data = tosave, mapping = aes( x, y, color= VISHcolor )) + xlim( min(toGraph$x), max(toGraph$x)) + ylim( min(toGraph$y), max(toGraph$y)) + theme_void() + geom_point(size= 5, show.legend = FALSE , alpha = 1) + scale_colour_gradientn( colours = viridis, limits=c( min(toGraph$VISHcolor) , max(toGraph$VISHcolor)))
@@ -17,4 +38,5 @@ for(n in Nzlayers){
   counter <- counter + 1
 }
 
-p <- grid.arrange( plotsHere[[1]] , plotsHere[[2]], plotsHere[[3]], ncol = 3, top= paste(gene))
+# This is to plot the three layers (AMPs, Epithelium, PE) next to each other 
+grid.arrange( plotsHere[[1]] , plotsHere[[2]], plotsHere[[3]], ncol = 3, top= paste(gene))
